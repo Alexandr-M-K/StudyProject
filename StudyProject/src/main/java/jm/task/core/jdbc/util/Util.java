6aca7973e041fd066c915a91c71beeeec3a5b280
Alexandr-M-K <death25031995@gmail.com>
2022-11-22 16:10:52 +0300

    1.1.4(0.1)

---
+++ StudyProject/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,33 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Util {
+    public static final String USER_NAME = "root";
+    public static final String PASSWORD = "root";
+    public static final String URL = "jdbc:mysql://localhost:3306/mysql";
+    public static Statement statement;
+    public static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    static {
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+}
