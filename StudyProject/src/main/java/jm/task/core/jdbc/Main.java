88fb1ceafd361411fc652c34916c09a8c458eb1f
Alexandr-M-K <death25031995@gmail.com>
2022-11-23 22:37:18 +0300

    PP - 1.1.4

--- StudyProject/src/main/java/jm/task/core/jdbc/Main.java
+++ StudyProject/src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable(); // 1.
+
+        userDao.saveUser("Sasha","Marienko",(byte)2);
+        userDao.saveUser("Lesha","Ivanov",(byte)3);
+        userDao.saveUser("Pasha","Sidorov",(byte)4);
+        userDao.saveUser("Anton","Popov", (byte)5);
+
+        var list = userDao.getAllUsers();
+        System.out.printf(list.toString());
+
+        userDao.cleanUsersTable();
+
+        userDao.dropUsersTable();
 
     }
 }
--- StudyProject/src/main/java/jm/task/core/jdbc/util/Util.java
+++ StudyProject/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,16 +1,13 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Util {
     public static final String USER_NAME = "root";
     public static final String PASSWORD = "root";
     public static final String URL = "jdbc:mysql://localhost:3306/mysql";
-    public static Statement statement;
     public static Connection connection;
+    public static Statement statement;
 
     static {
         try {
